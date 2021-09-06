0e286dca0e7b9cd0c20b8b97c502891316725178
iuliachebykina <julia.chebykina.13@gmail.com>
2021-09-06 19:07:49 +0500

    r

--- src/main/java/ru/renue/testTask/service/CsvParserService.java
+++ src/main/java/ru/renue/testTask/service/CsvParserService.java
@@ -4,6 +4,7 @@ package ru.renue.testTask.service;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class CsvParserService {
@@ -11,30 +12,6 @@ public class CsvParserService {
 
 
     public List<String[]> getAirports(List<String[]> data, String substring, Integer column) {
-        List<String[]> airports = new ArrayList<>();
-
-        int index= data.size()/2+1;
-        while (index < data.size() && index >= 0){
-            if(data.get(index)[column].substring(0, substring.length()).compareTo(substring) < 0){
-                index += index/2+1;
-            } else if(data.get(index)[column].substring(0, substring.length()).compareTo(substring) > 0){
-                index -= index/2+1;
-            } else {
-                break;
-            }
-        }
-        if(index < 0 || index >= data.size()){
-            return airports;
-        }
-        while (index-1 >= 0 && data.get(index-1)[column].startsWith(substring)){
-            index--;
-        }
-
-        while (index < data.size() && data.get(index)[column].startsWith(substring)){
-            airports.add(data.get(index));
-            index++;
-        }
-        return airports;
-
+        return data.stream().filter(a-> a[column].startsWith(substring)).collect(Collectors.toList());
     }
 }
